4a12f1414c18ad53a49deb035e49d1f2c0df91fb
Klayton <kokanee4@gmail.com>
2026-09-11 15:08:01 -0400

    Completed my task manager styling and functionality

--- script.js
+++ script.js
@@ -20,18 +20,30 @@ form.addEventListener("submit", function(event) {
     };
     tasks.push(task);
     displayTasks();
+    taskInput.value = "";
 });
 
 function displayTasks() {
 
-taskList.innerHTML = "";
+    taskList.innerHTML = "";
 
     for(let i = 0; i < tasks.length; i++) {
         const task = tasks[i];
 
         const taskElement = document.createElement("div");
+        taskElement.classList.add("task");
+        if(task.priority === "Low") {
+            taskElement.classList.add("Low");
+        } else if(task.priority === "Medium") {
+            taskElement.classList.add("Medium");
+        } else if(task.priority === "High") {
+            taskElement.classList.add("High");
+        }
         const completeButton = document.createElement("button");
+        completeButton.classList.add("complete-button");
         const deleteButton = document.createElement("button");
+        deleteButton.classList.add("delete-button");
+
         if(task.completed) {
             taskElement.classList.add("completed");
         }
@@ -42,13 +54,15 @@ taskList.innerHTML = "";
         completeButton.addEventListener("click", function() {
             task.completed = true;
             taskElement.classList.add("completed");
-        })
+        });
         deleteButton.addEventListener("click", function() {
             tasks.splice(i, 1);
             displayTasks();
-        })
+        });
+        const lineBreak = document.createElement("br");
 
         taskList.appendChild(taskElement);
+        taskElement.appendChild(lineBreak);
         taskElement.appendChild(completeButton);
         taskElement.appendChild(deleteButton);
     }
